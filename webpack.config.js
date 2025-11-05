1d861ed4370b60a6fb06252662aeed0f330932ec
anwar907 <rilltheend@gmail.com>
2025-11-06 01:12:37 +0700

    fix workflow github action

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: process.env.NODE_ENV === 'production' ? '/anwar907.github.io/' : '/',
+    publicPath: '/',
     clean: false
   },
   performance: {
